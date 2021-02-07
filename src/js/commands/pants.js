70432497fd3ba5e1f2b0e88e9866a9e9efb06ef0
Seabagel <hsrt1367tim@gmail.com>
2021-02-07 11:39:00 -0800

    Added pants command

---
+++ src/js/commands/pants.js
@@ -0,0 +1,60 @@
+module.exports = {
+  name: "pants",
+  execute(userInput) {
+    pantsCommand(userInput, this.name);
+  },
+};
+
+const pantsCommand = async (userInput, thisName) => {
+  // Dependencies
+  const { MessageEmbed } = require("discord.js");
+  const cheerio = require("cheerio");
+  const { message_tools, http_tools, math_tools } = require("../tools");
+  // Data
+  const { texts, images, pants } = require("../JSON_helper");
+
+  try {
+    // Static link to search engines
+    let picURL = {
+      uri: `https://results.dogpile.com/serp?q=${math_tools.randArrayValue(
+        pants.pantsType
+      )}&page=${1 + math_tools.randomIntEx(4)}&sc=YPgyHRjAO7ux00`,
+      method: "GET",
+      headers: {
+        Accept: "text/html",
+        "User-Agent": "Chrome",
+      },
+      transform: (body) => cheerio.load(body),
+    };
+
+    // Search result and grab a random picture
+    let pictureWebpage = http_tools
+      .requestPage(picURL)
+      .then((response) => {
+        // Returns a Promise of picture URL
+        let urls = response(".image a.link");
+        let picture = urls.eq(math_tools.randomIntEx(urls.length)).attr("href");
+        if (!urls.length) return console.log("Can't get pictures");
+        return { picture };
+      })
+      .catch((err) => message_tools.catchError(userInput));
+
+    await Promise.all([pictureWebpage]).then((res) => {
+      const resPicture = res[0].picture;
+
+      // Assemble embedded message
+      let embedded = new MessageEmbed()
+        .setColor("E67E22")
+        .setAuthor("Google Images", images.googleAvatar)
+        .setImage(resPicture)
+        .addField(texts.empty, message_tools.github(thisName))
+        .setFooter(texts.footerText, images.githubIcon);
+
+      // Send message
+      userInput.channel.send(embedded);
+      message_tools.logCommand(userInput, thisName);
+    });
+  } catch (error) {
+    message_tools.catchError(userInput, error);
+  }
+};
--- src/js/commands/time.js
+++ src/js/commands/time.js
@@ -1,105 +1,107 @@
 module.exports = {
-    name: "time",
-    execute(userInput, args) {
-        timeCommand(userInput, args, this.name);
-    },
+  name: "time",
+  execute(userInput, args) {
+    timeCommand(userInput, args, this.name);
+  },
 };
 
 const timeCommand = async (userInput, args, thisName) => {
-    // Dependencies
-    const { MessageEmbed } = require("discord.js");
-    const cheerio = require("cheerio");
-    const {
-        message_tools,
-        http_tools,
-        text_tools,
-        math_tools,
-    } = require("../tools");
-    // Data
-    const { texts, images } = require("../JSON_helper");
+  // Dependencies
+  const { MessageEmbed, ReactionUserManager } = require("discord.js");
+  const cheerio = require("cheerio");
+  const {
+    message_tools,
+    http_tools,
+    text_tools,
+    math_tools,
+  } = require("../tools");
+  // Data
+  const { texts, images } = require("../JSON_helper");
 
-    try {
-        // Join args for URL
-        let argsURL = args.join("+");
-        let argsString = text_tools.capitalize(args.join(" "));
-
-        // Static link to search engines
-        let timeURL = `https://www.google.com/search?q=what+time+is+it+in+${argsURL}`;
-        let picURL = {
-            uri: `https://results.dogpile.com/serp?qc=images&q=${argsURL}`,
-            method: "GET",
-            headers: {
-                Accept: "text/html",
-                "User-Agent": "Chrome",
-            },
-            transform: (body) => cheerio.load(body),
-        };
-
-        // Search result which grabs timezone, date, and time
-        let timeWebpage = http_tools
-            .requestPage(timeURL)
-            .then((res) => {
-                // Returns a Promise of object containing date/time
-                let $ = cheerio.load(res);
-                let dateTimezone = $("span:nth-child(1)")
-                    .eq(7)
-                    .text()
-                    .split("\n");
-                let time = $("div:nth-child(1)").eq(21).text();
-                return { dateTimezone, time };
-            })
-            .catch((err) => message_tools.catchError(userInput));
-
-        // Search result and grab a random picture
-        let pictureWebpage = http_tools
-            .requestPage(picURL)
-            .then((response) => {
-                // Returns a Promise of picture URL
-                let urls = response(".image a.link");
-                let picture = urls
-                    .eq(math_tools.randomIntEx(urls.length))
-                    .attr("href");
-                if (!urls.length) return console.log("Can't get pictures");
-                return { picture };
-            })
-            .catch((err) => message_tools.catchError(userInput));
-
-        await Promise.all([timeWebpage, pictureWebpage]).then((res) => {
-            const resHour = res[0].time;
-            const resLocation = res[0].dateTimezone[1];
-            const resDate = res[0].dateTimezone[0];
-            const resPicture = res[1].picture;
-
-            // Assemble embedded message
-            let embedded = new MessageEmbed()
-                .setColor("dedede")
-                .setAuthor("Google Search", images.googleAvatar)
-                .setTitle(argsString)
-                .addFields(
-                    {
-                        name: resHour,
-                        value: resLocation,
-                    },
-                    {
-                        name: "Date:",
-                        value: resDate,
-                    }
-                )
-                .setImage(resPicture)
-                .addField(texts.empty, message_tools.github(thisName))
-                .setFooter(texts.footerText, images.githubIcon);
-
-            // Set different thumbnail based on Time-of-day
-            const isNight = resHour.endsWith("PM");
-            !isNight
-                ? embedded.setThumbnail(images.thumbnails.timeDayCommand)
-                : embedded.setThumbnail(images.thumbnails.timeNightCommand);
-
-            // Send message
-            userInput.channel.send(embedded);
-            message_tools.logCommand(userInput, thisName);
-        });
-    } catch (error) {
-        message_tools.catchError(userInput, error);
+  try {
+    // Don't let people use time bot to look up pants
+    const wordList = ["pant", "trouser", "short", "jean", "legging", "brief"];
+    if (text_tools.containsWord(args[0], wordList)) {
+      message_tools.catchError(userInput, "Pants Error");
+      return;
     }
+
+    // Join args for URL
+    let argsURL = args.join("+");
+    let argsString = text_tools.capitalize(args.join(" "));
+
+    // Static link to search engines
+    let timeURL = `https://www.google.com/search?q=what+time+is+it+in+${argsURL}`;
+    let picURL = {
+      uri: `https://results.dogpile.com/serp?qc=images&q=${argsURL}`,
+      method: "GET",
+      headers: {
+        Accept: "text/html",
+        "User-Agent": "Chrome",
+      },
+      transform: (body) => cheerio.load(body),
+    };
+
+    // Search result which grabs timezone, date, and time
+    let timeWebpage = http_tools
+      .requestPage(timeURL)
+      .then((res) => {
+        // Returns a Promise of object containing date/time
+        let $ = cheerio.load(res);
+        let dateTimezone = $("span:nth-child(1)").eq(7).text().split("\n");
+        let time = $("div:nth-child(1)").eq(21).text();
+        return { dateTimezone, time };
+      })
+      .catch((err) => message_tools.catchError(userInput));
+
+    // Search result and grab a random picture
+    let pictureWebpage = http_tools
+      .requestPage(picURL)
+      .then((response) => {
+        // Returns a Promise of picture URL
+        let urls = response(".image a.link");
+        let picture = urls.eq(math_tools.randomIntEx(urls.length)).attr("href");
+        if (!urls.length) return console.log("Can't get pictures");
+        return { picture };
+      })
+      .catch((err) => message_tools.catchError(userInput));
+
+    await Promise.all([timeWebpage, pictureWebpage]).then((res) => {
+      const resHour = res[0].time;
+      const resLocation = res[0].dateTimezone[1];
+      const resDate = res[0].dateTimezone[0];
+      const resPicture = res[1].picture;
+
+      // Assemble embedded message
+      let embedded = new MessageEmbed()
+        .setColor("dedede")
+        .setAuthor("Google Search", images.googleAvatar)
+        .setTitle(argsString)
+        .addFields(
+          {
+            name: resHour,
+            value: resLocation,
+          },
+          {
+            name: "Date:",
+            value: resDate,
+          }
+        )
+        .setImage(resPicture)
+        .addField(texts.empty, message_tools.github(thisName))
+        .setFooter(texts.footerText, images.githubIcon);
+
+      // Set different thumbnail based on Time-of-day
+      const isNight = resHour.endsWith("PM");
+      !isNight
+        ? embedded.setThumbnail(images.thumbnails.timeDayCommand)
+        : embedded.setThumbnail(images.thumbnails.timeNightCommand);
+
+      // Send message
+      userInput.channel.send(embedded);
+      message_tools.logCommand(userInput, thisName);
+    });
+  } catch (error) {
+    message_tools.catchError(userInput, error);
+  }
 };
