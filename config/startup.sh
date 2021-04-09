433bbba26351116f40e9fe235703213e0d6878d3
Seabagel <hsrt1367tim@gmail.com>
2021-04-09 01:02:03 -0700

    consolidated startup

--- config/startup.sh
+++ config/startup.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-cd $HOME/github/Discord-MamaSlimes/
+## 1-time setup
 
-git fetch
+# Add script to rc.local
+# sudo nano /etc/rc.local
+# /home/pi/startup.sh &
+
+# Copy script from github to home
+# cp /home/pi/github/Discord-MamaSlimes/startup.sh /home/pi/startup.sh
+# sudo chmod u+x /home/pi/startup.sh
+
+cd $HOME/github/Discord-MamaSlimes/
 
 git pull
 
--- config/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp /home/pi/github/Discord-MamaSlimes/startup.sh /home/pi/startup.sh
-
-sudo chmod u+x /home/pi/startup.sh
-
-exit 0
