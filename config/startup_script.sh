7c15237c471df78207dcbeca5dde6c176efb7c4f
Seabagel <hsrt1367tim@gmail.com>
2021-04-08 23:25:58 -0700

    Update startup_script.sh

--- config/startup_script.sh
+++ config/startup_script.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
+
+## Update repo
 cd $HOME/github/Discord-MamaSlimes/
 git fetch
 git pull
+
+## Update the startup script
+sudo cp $HOME/github/Discord-MamaSlimes/config/startup_script.sh $HOME/github/startup_script.sh
+sudo chmod +x $HOME/github/startup_script.sh
+
+## Start Nodejs App
 npm i
 npm run start &
 disown -h
 exit
 
+## Uncommit / Flush files
 # rm -rf *
 # git reset --hard
 
-# sudo cp $HOME/github/Discord-MamaSlimes/config/startup_script.sh $HOME/github/startup_script.sh
-# sudo chmod +x $HOME/github/startup_script.sh
-# sh $HOME/github/startup_script.sh
-
+## Startup Chrontab
 # crontab -u pi -e
 # @reboot sh $HOME/github/startup_script.sh
-# exit
