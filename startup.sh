b459780853a4e649d4d8cf5deacb041b0a688cc2
Seabagel <hsrt1367tim@gmail.com>
2021-04-09 01:03:29 -0700

    move file

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@
 # /home/pi/startup.sh &
 
 # Copy script from github to home
-# cp /home/pi/github/Discord-MamaSlimes/startup.sh /home/pi/startup.sh
+# sudo cp /home/pi/github/Discord-MamaSlimes/startup.sh /home/pi/startup.sh
 # sudo chmod u+x /home/pi/startup.sh
 
 cd $HOME/github/Discord-MamaSlimes/
