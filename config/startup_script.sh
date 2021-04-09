348e78aa36f23f48cbcc358685f4294ef2c4bca5
Seabagel <hsrt1367tim@gmail.com>
2021-04-09 00:06:18 -0700

    Update startup_script.sh

--- config/startup_script.sh
+++ config/startup_script.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
 # https://raspberrypi.stackexchange.com/questions/15475/run-bash-script-on-startup
+# sh $HOME/startup_script.sh
 
 ## Update repo
 cd $HOME/github/Discord-MamaSlimes/
 git fetch
 git pull
-
-## Update the startup script
-# sudo nano .bashrc
-# Scroll down to the bottom and add the line: ./startup_script.sh
-sudo cp $HOME/github/Discord-MamaSlimes/config/startup_script.sh $HOME/startup_script.sh
-
 ## Start Nodejs App
-cd $HOME/github/Discord-MamaSlimes/
 npm i
 npm run start &
 disown -h
@@ -22,3 +16,8 @@ exit
 ## Uncommit / Flush files
 # rm -rf *
 # git reset --hard
+
+## Update the startup script
+# sudo nano .bashrc
+# Scroll down to the bottom and add the line: ./startup_script.sh
+# sudo cp $HOME/github/Discord-MamaSlimes/config/startup_script.sh $HOME/startup_script.sh
