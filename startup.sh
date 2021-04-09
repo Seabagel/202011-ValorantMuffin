6d78434abb880a4b4bb9f3f78f35735ba8b200ef
Seabagel <hsrt1367tim@gmail.com>
2021-04-09 01:15:31 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,16 +2,17 @@
 
 ## 1-time setup
 
-# Add script to rc.local
+## Add script to rc.local
 # sudo nano /etc/rc.local
 # /home/pi/startup.sh
 
-# Copy script from github to home
+## Copy script from github to home
 # sudo cp /home/pi/github/Discord-MamaSlimes/startup.sh /home/pi/startup.sh
 # sudo chmod u+x /home/pi/startup.sh
+
 ## sh /home/pi/startup.sh
 
-# Finally, you must reboot your system using $ sudo reboot
+## Finally, you must reboot your system using $ sudo reboot
 # When you starts up the computer your script is completely work.
 
 cd $HOME/github/Discord-MamaSlimes/
