6e35c4a67d6aa291e063757dd74e49a69ae71e86
Seabagel <hsrt1367tim@gmail.com>
2021-04-08 23:54:14 -0700

    Update startup_script.sh

--- config/startup_script.sh
+++ config/startup_script.sh
@@ -6,11 +6,11 @@ git fetch
 git pull
 
 ## Update the startup script
-sudo cp $HOME/github/Discord-MamaSlimes/config/startup_script.sh /etc/init.d/startup_script.sh
-sudo chmod +x /etc/init.d/startup_script.sh
-sudo update-rc.d startup_script.sh defaults
+sudo cp $HOME/github/Discord-MamaSlimes/config/startup_script.sh $HOME/startup_script.sh
+sudo chmod +x $HOME/startup_script.sh
 
 ## Start Nodejs App
+cd $HOME/github/Discord-MamaSlimes/
 npm i
 npm run start &
 disown -h
@@ -23,3 +23,7 @@ exit
 ## Startup Chrontab
 # crontab -u pi -e
 # @reboot sh /etc/init.d/startup_script.sh
+
+## bashrc
+# sudo nano .bashrc
+# Scroll down to the bottom and add the line: ./startup_script.sh
